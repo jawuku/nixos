b969ec8d59cc04a291fafd0b3765f08def666442
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2025-03-26 10:50:21 +0000

    add aliases to git.nix

--- home-modules/git.nix
+++ home-modules/git.nix
@@ -5,6 +5,11 @@
     enable = true;
     userName = "jawuku";
     userEmail = "j.awuku@gmail.com";
+    aliases = {
+      cm = "commit";
+      co = "checkout";
+      s = "status";
+    };
     extraConfig = {
       core = {
         editor = "vim";
